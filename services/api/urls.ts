6f7e6df2019bbc06661912a63a26ac844fbc5d61
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-12-10 21:02:17 +0700

    fix img cloud

--- services/api/urls.ts
+++ services/api/urls.ts
@@ -1,6 +1,8 @@
 const BASEURL =
   process.env.NEXT_PUBLIC_API_ENDPOINT || 'https://linen-a-be.herokuapp.com/v1';
 
+// const BASEURL = 'http://localhost:5000/v1';
+
 export const API_URL = {
   home: BASEURL,
   adminLogin: `${BASEURL}/account/login/`,
